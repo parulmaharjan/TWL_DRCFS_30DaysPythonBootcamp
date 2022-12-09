5731baa3b41ab08dab88d504e8cc9fb7a04ed8c1
parul <parulmhrzn@gmail.com>
2022-12-09 16:35:52 +0545

    assignment 1&2

--- Week 1/personal/MyAssignments/guessgame.py
+++ Week 1/personal/MyAssignments/guessgame.py
@@ -1,17 +1,21 @@
 
 import random
-randomnumber=print(random.randint(1,30));
-
-for i in range(5):
-    userInput=int(input('your input:'));
+randomnumber=random.randint(1,30)
+print('welcome to guessing game!!! Guess the number in 5 tries')
+for i in range(1,6):
+    print("you have total", 6-i, "tries remaining out of 5")
+    userInput=int(input(' Enter your input between 1 and 30:'));
     if userInput==randomnumber:
-        print("you won you are a good guesser");
-
+        print("*****correct guess,you won***** ");
     elif userInput>randomnumber:
-        print("your guess is greater");
+        print("*****your guess is greater*****");
     elif userInput<randomnumber:
-        print("your guess is low");    
+        print("*****your guess is low*****");  
+print("actual number to be guessed:",randomnumber)      
+if i == 5:
+    print("***** Try again!!!You're a bad guesser*****")
         
+    
+  
 
-
-
+    
